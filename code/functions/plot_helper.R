dd49fac835b2808fba4afb137513c78cbf1a8bbf
joonho112 <joonho@berkeley.edu>
2018-08-08 20:33:19 -0700

    Add figures

--- code/functions/plot_helper.R
+++ code/functions/plot_helper.R
@@ -56,7 +56,7 @@ shape_palette <- c(16, 17, 15, 18, 1, 2, 0, 5, 6, 4, 3, 8, 10, 7, 9)
 ###'     - No facets
 ###'     
 
-### Define function 'plot_trend_0fac'
+### Define function
 plot_trend_xy <- function(dataframe, 
                           x, 
                           y,
@@ -219,5 +219,116 @@ plot_trend_grp_facet <- function(dataframe,
 # 
 # plot_trend_grp_facet(df_plot, Fiscalyear, mean_value, Dtype,
 #                      key~., "free_y")
+
+
+
+###'######################################################################
+###'
+###' operation14(): Remove districts with insufficient years of data
+###' 
+###' => Analyze only traditional schools in elementary, high, and unified 
+###'    school districts that have been in continuous operation (14 years) 
+###'    in California from 2003 through 2017
+
+setwd(work_dir)
+load(file = "data/years_of_operation.rda")   # Data dependency: years_of_operation.csv
+
+operation14 <- function(df){
+  df %>%
+    left_join(select(years_of_operation, -Dname, -Dtype), by = c("Ccode", "Dcode")) %>%
+    filter(opr_years == 14)
+}
+
+
+
+###'######################################################################
+###'
+###' get_weighted_mean(): Get weighted district averages
+###' 
+###' 
+
+get_weighted_mean <- function(df, 
+                              x = Fiscalyear, 
+                              y = sum_value_PP_16, 
+                              weight = K12ADA_C, 
+                              ...){
+  
+  ### Enquote variables
+  x <- enquo(x)
+  y <- enquo(y)
+  weight <- enquo(weight)
+  group_var <- quos(...)
+  
+  df %>% 
+    group_by(!!x, !!!group_var) %>%
+    summarise(mean_value = round(weighted.mean(!!y, !!weight, na.rm = TRUE), 0))
+}
+
+
+
+###'######################################################################
+###'
+###' Calculate the y-limits and height for the PDF file
+###' 
+###' 
+
+auto_ylim <- function(value_vec = NULL){
+  
+  ### The optimal y-limits
+  bottom <- min(value_vec) - (min(value_vec) - 0)/5
+  ceiling <- max(value_vec) + (min(value_vec) - 0)/5
+  
+  ### Return objects
+  auto_ylim <- c(bottom, ceiling)
+  return(auto_ylim)
+}
+
+auto_height <- function(factor_vec = NULL){
+  
+  ### The height for the PDF file
+  num_factor <- length(levels(factor_vec))
+  height <- ifelse(num_factor <= 4, 6, num_factor + 3)
+  
+  ### Return objects
+  return(height)
+}
+
+
+
+###'######################################################################
+###'
+###' Calculate percentages based on groups
+###'
+###'
+
+group_percent <- function(df, 
+                          value = mean_value, 
+                          ...){
+  
+  # Enquote variables
+  value <- enquo(value)
+  group <- quos(...)
+
+  
+  #' (1) Calculate the percentages
+  #' (2) Format the labels and calculate their positions
+  df %>%
+    group_by(!!!group) %>%
+    mutate(group_sum = sum(!!value, na.rm = TRUE), 
+           percent = !!value/group_sum * 100, 
+           # don't need to calculate the label positions from ggplot 2.1.0 
+           # position = cumsum(amount) - 0.5 * amount,  
+           label_text = paste0(sprintf("%.0f", percent), "%")) -> df
+  return(df)
+}
+
+
+
+
+
+
+
+
+
 
 
