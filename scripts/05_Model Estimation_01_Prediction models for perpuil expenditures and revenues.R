19bb921cbe7c481ea19a45cba004e3d6f4216715
joonho112 <joonho@berkeley.edu>
2018-08-21 19:56:30 -0700

    update modeling

--- scripts/05_Model Estimation_01_Prediction models for perpuil expenditures and revenues.R
+++ scripts/05_Model Estimation_01_Prediction models for perpuil expenditures and revenues.R
@@ -44,6 +44,9 @@ data_dir <- c("D:/Data/LCFF")
 ### Call libraries
 library(tidyverse)
 library(car)
+library(plm)
+library(lme4)
+library(sjPlot)
 
 
 ### Call functions
@@ -137,40 +140,207 @@ ggplot(df, aes(formula_weight)) +
   geom_density()
 
 
+### Generate decile dummies for simulated IV
+
+df <- df %>%
+  mutate(decile = ntile(SimIV, 10))
+
+table(df$decile)
+
+
 
 ###'######################################################################
 ###'
-###' The first stage model predicting average per-pupil spending
+###' Merge FY1314 funding snapshot to per-pupil expenditure panel data
 ###' 
-###' in district "d" in year "t"
-###'
-###'
-###'
+###' 
 
-### Merge FY1314 funding snapshot to per-pupil expenditure panel data
-
+### Extract dataframe
 assign(names(list_expenditures_def_1)[[1]], list_expenditures_def_1[[1]])
 
+
+### Rename CountyCode and DistrictCode and convert them to numeric
 df <- df %>%
   mutate(Ccode = as.numeric(CountyCode), 
          Dcode = as.numeric(DistrictCode)) %>%
   select(Ccode, Dcode, everything()) %>%
   select(-(CountyCode:DistrictCode))
 
+
+### Merge the two datasets
 total_exp <- left_join(total_exp, df, by = c("Ccode", "Dcode"))
 
 
-###  
-
-p_trend <- scatterplot(sum_value_PP_16 ~ Fiscalyear, 
-                       boxplots = FALSE, 
-                       smooth = TRUE, 
-                       reg.line = FALSE, 
-                       data = total_exp)
+### Log transformation of outcome variables
+total_exp <- total_exp %>%
+  mutate(log_y = if_else(sum_value_PP_16 == 0, 0, log(sum_value_PP_16)))
 
 
 
+###'######################################################################
+###'
+###' Fitting piecewise growth curve models:
+###' 
+###' => Provide visual evidence that the funding formula weight 
+###' 
+###'    does not predict changes in funding levels in the previous years
+###' 
+###'    leading up to the LCFF policy change in 2013
+###'
+###' Level 1: 
+###' Y_{ij} = beta_{0j} + beta_{1j} time_1 + beta_{2j} time_2 + r_{ij}
+###'
+###' Level 2:
+###' beta_{0j} = gamma_{00} + gamma_{01} formula_weight_{j} + U_{0j}
+###' beta_{1j} = gamma_{10} + gamma_{11} formula_weight_{j} + U_{1j}
+###' beta_{2j} = gamma_{20} + gamma_{21} formula_weight_{j} + U_{2j}
+###'
+###' Time coding scheme:
+###' time_1: the pre-recession housing bubble (2003-2006) 
+###' time_2: the housing crash and ensuing recession (2007-2009 + 2009-2012)
+###' time_3: LCFF years (2013-2016)
+###'
+###' time_1: c(0, 1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3)
+###' time_2: c(0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 6, 6, 6, 6)
+###' time_3: c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4)
+###'
+###'
 
+### Initial exploratory plot
+df_plot <- total_exp %>%
+  filter(Dtype %in% c("ELEMENTARY", "HIGH", "UNIFIED"))
+  
+p <- ggplot(aes(x = Fiscalyear, y = log_y), data = df_plot) +
+  geom_line(aes(group = Dcode), color = "gray") + 
+  geom_smooth(aes(group = 1), size = 3, color = "red", se = FALSE) +
+  facet_grid(.~ Dtype) + 
+  theme_trend + 
+  scale_x_continuous(breaks = seq(min(df_plot$Fiscalyear), 
+                                  max(df_plot$Fiscalyear), 
+                                  by = 1))
+
+ggsave("figures/total_exp_growth.pdf", p, width = 15, height = 8)
+
+
+### Generate time variables for piecewise regression
+Fiscalyear <- 2003:2016
+time_1 <- c(0, 1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3)
+time_2 <- c(0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 6, 6, 6, 6)
+time_3 <- c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4)
+time_df <- data.frame(Fiscalyear, time_1, time_2, time_3)
+
+total_exp <- left_join(total_exp, time_df, by = c("Fiscalyear")) 
+
+
+### Fit the piecewise growth curve model
+
+fit_piece <- lmer(log_y ~ (time_1 + time_2 + time_3) * formula_weight + 
+                    (time_1 + time_2 + time_3 | Dcode), 
+                  data = total_exp, REML = FALSE)
+
+summary(fit_piece)
+
+
+### Graph fitted curves
+
+FittedFE <- function(x) model.matrix(x) %*% fixef(x)
+
+df_plot_fit <- data.frame(fit_piece@frame, fitted = FittedFE(fit_piece))
+
+df_plot_fit <- df_plot_fit %>% arrange(Dcode)
+
+df_plot_fit <- left_join(df_plot_fit, time_df, by = c("time_1", "time_2", "time_3"))
+
+p <- ggplot(df_plot_fit, aes(x = Fiscalyear, y = log_y)) + 
+  # geom_point(shape = 19) +
+  stat_summary(fun.y = mean, geom = "point", size = 5, shape = 1) + 
+  geom_line(aes(y = fitted), lwd = 1.5)
+
+
+
+###'######################################################################
+###'
+###' The first stage model predicting average per-pupil spending
+###' 
+###' in district "d" in year "t"
+###' 
+###' - (1) event time dummies
+###' - (2) formula weight dummies Q_{2013}  
+###' 
+###' => The coefficients for the two-way interactions map out
+###'    the dynamic treatment effects of the LCFF reform on per-pupil spending
+###'    for districts in spending quantile Q_{2013}
+###' 
+###' - (3) district fixed-effect
+###'
+###'
+
+### Convert to factors and set reference category
+df <- total_exp
+
+df$Fiscalyear <- factor(df$Fiscalyear)
+df$decile <- factor(df$decile)
+df$Fiscalyear <- relevel(df$Fiscalyear, "2013")
+
+
+### Tidy up analytical sample
+df <- total_exp %>%
+  filter(Dtype %in% c("ELEMENTARY", "HIGH", "UNIFIED")) %>%
+  select(log_y, decile, Fiscalyear, Dcode) 
+
+
+### Filter only complete cases
+df <- df[complete.cases(df), ]
+
+
+### Fixed effects using least squares dummy variable model
+
+df$Fiscalyear <- factor(df$Fiscalyear)
+df$decile <- factor(df$decile)
+df$Fiscalyear <- relevel(df$Fiscalyear, "2013")
+
+mm <- model.matrix(~ decile*Fiscalyear, df)
+write.csv(mm, file = "tables/model_matrix.csv")
+
+fit_lm <-lm(log_y ~ mm + factor(Dcode) + 0, data = df)
+summary(fit_lm)
+
+
+### Plot dynamic treatment effects
+
+coef <- data.frame(fit_lm$coefficients)
+
+coef$coef <- rownames(coef)
+
+rownames(coef) <- NULL
+
+coef_int <- coef[grepl(":", coef$coef), ]
+
+coef_int <- coef_int %>%
+  separate(coef, c("decile", "year"), ":") %>%
+  select(decile, year, fit_lm.coefficients) %>%
+  rename(coef = fit_lm.coefficients) %>% 
+  arrange(decile, year)
+  
+coef_int$year <- as.numeric(substr(coef_int$year, 11, 14))
+
+
+coef_int$decile <- factor(gsub('\\D+','', coef_int$decile), 
+                          levels = rev(as.character(2:10)))
+coef_int$coef <- round(coef_int$coef, 2)
+
+
+coef_2013 <- data.frame(decile = as.character(2:10), 
+                        year = rep(2013, 9), 
+                        coef = rep(0, 9))
+
+coef_int <- bind_rows(coef_int, coef_2013)
+
+
+p1 <- plot_trend_grp(coef_int, year, coef, decile, yline = 2013, 
+                     ylim = auto_ylim(coef_int$coef))
+
+p1
 
 
 
